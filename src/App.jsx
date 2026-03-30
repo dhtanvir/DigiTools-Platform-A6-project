8c169711853d2a01d6859bf25570970e1096a3b9
MD Tanvirul Islam <57485202+dhtanvir@users.noreply.github.com>
2026-03-30 14:45:17 +0600

    Pricing section upload done

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,26 @@
-
-import './App.css'
-import Banner from './component/Banner/Banner'
-import BannerCount from './component/Banner/BannerCount'
-import Navbar from './component/Navbar/Navbar'
+import "./App.css";
+import Banner from "./component/Banner/Banner";
+import BannerCount from "./component/Banner/BannerCount";
+import Navbar from "./component/Navbar/Navbar";
+import PricingCard from "./component/Pricing/PricingCard";
+import StepsCard from "./component/Steps/StepsCard";
 
 function App() {
-  
-
   return (
     <>
-    
-
-    <header>
-      <Navbar />
-    </header>
-    <main>
-      <Banner/>
-      <BannerCount/>
-
-    </main>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <Banner />
+        <BannerCount />
+        {/*  */}
+        {/*  */}
+        <StepsCard />
+        <PricingCard />
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
